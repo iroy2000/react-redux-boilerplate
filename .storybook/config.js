4a43d15ae56f3f16ebc0411d49067ae074943e44
Roy Yu <roy@chegg.com>
2018-01-18 18:13:46 -0800

    add storybook support back

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,9 +1,9 @@
 import { configure } from '@storybook/react';
 
-const req = require.context('../stories', true, /\.js$/)
-
+// automatically import all files ending in *.stories.js
+const req = require.context('../stories', true, /.stories.js$/);
 function loadStories() {
-  req.keys().forEach(filename => req(filename))
+  req.keys().forEach((filename) => req(filename));
 }
 
 configure(loadStories, module);
